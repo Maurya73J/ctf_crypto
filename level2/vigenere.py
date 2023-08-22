d67d4af0431f2430c03fc104d1abcd0370762022
Maurya73J <mauryaj20@iitk.ac.in>
2023-08-22 08:23:01 +0530

    l23
    l23

---
+++ level2/vigenere.py
@@ -0,0 +1,3 @@
+import pycipher
+plaintext = pycipher.Vigenere('JCJCFFCCCB').decipher("Kg fcwd qh vin pnzy hjcocnt, cjjwg ku wnth nnyvng kxa cjjwg. Urfjm xwy yjg rbbufqwi \"vjg_djxn_ofs_dg_rmncbgi\" yq iq uqtxwlm. Oca zxw qcaj vjg tctnplyj hqs cjn pjcv ejbvdnt. Yt hkpe cjn gcnv, aqv okauy bknn ongm vt zvvgs vcpkh bqtft cjntj.")
+print(plaintext)
